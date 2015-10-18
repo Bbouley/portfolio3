59cac88667529cf3da91f21ea0390b8fa39c7c62
bbouley <b.bouley@hotmail.co.uk>
2015-10-17 19:43:09 -0600

    added new project into folder

--- js/angular.js
+++ js/angular.js
@@ -3,5 +3,25 @@
 var app = angular.module('myApp', []);
 
 app.controller('myController', function($scope) {
-  $scope.greeting = "Hello World!";
+
+  $scope.studyDescription = false;
+
+  $scope.studyDescriptionButton = function(){
+    if($scope.studyDescription === false){
+      $scope.studyDescription = true;
+    } else {
+      $scope.studyDescription = false;
+    }
+  };
+
+  $scope.playlistDescription = false;
+
+  $scope.playlistDescriptionButton = function(){
+    if($scope.playlistDescription === false){
+      $scope.playlistDescription = true;
+    } else {
+      $scope.playlistDescription = false;
+    }
+  };
+
 });
